4f49c5b6745f858ad3589ac717b281abb54fb658
lihui <lihui@hdsc.com>
2018-01-19 10:41:47 +0800

    分支开发完成

---
+++ src/main/java/com/example/eurekaserver/test/NewBranch.java
@@ -0,0 +1,10 @@
+package com.example.eurekaserver.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewBranch {
+  public void test(){
+        System.out.println("@@");
+    }
+}
